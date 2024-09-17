e0044565b49b4130782b048fd577bd123ccf3731
RAGL90 <ricgarlue@gmail.com>
2024-09-17 12:26:39 +0200

    Un env raro a ver si cuela

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,25 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "SpotMyPet-Backend",
+//       script: "app.js",
+//       cwd: "/home/appuser/Prj-SpotiPet-Back/src/",
+//       interpreter: "/home/appuser/.nvm/versions/node/v18.20.4/bin/node",
+//       watch: false,
+//       env: {
+//         NODE_ENV: "production",
+//         DATABASE_URL: process.env.DATABASE_URL,
+//         PORT: 9000,
+//         TOKEN_SECRET: process.env.TOKEN_SECRET,
+//         REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+//         // Otras variables de entorno
+//       },
+//       log_date_format: "YYYY-MM-DD HH:mm Z",
+//       merge_logs: true,
+//     },
+//   ],
+// };
+
 module.exports = {
   apps: [
     {
@@ -7,12 +29,7 @@ module.exports = {
       interpreter: "/home/appuser/.nvm/versions/node/v18.20.4/bin/node",
       watch: false,
       env: {
-        NODE_ENV: "production",
-        DATABASE_URL: process.env.DATABASE_URL,
-        PORT: 9000,
-        TOKEN_SECRET: process.env.TOKEN_SECRET,
-        REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-        // Otras variables de entorno
+        NODE_ENV: "production", // Solo define NODE_ENV
       },
       log_date_format: "YYYY-MM-DD HH:mm Z",
       merge_logs: true,
